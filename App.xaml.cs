54e4683f0c69a8bcd99748fe85caba664de00d2b
Andres Sigcha <s4npolas@gmail.com>
2026-06-01 21:15:52 -0500

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Kora.Mobile.Views;
+
+namespace Kora.Mobile;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+        MainPage = new NavigationPage(new LoginPage());
+    }
+}
---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Kora.Mobile")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Kora.Mobile.Pages")]
